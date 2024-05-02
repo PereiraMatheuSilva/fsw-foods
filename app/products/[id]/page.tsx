1072bb4afa8d8cbd9244fbd511bb353becfb3f64
Matheus Pereira <matheusp.dasilva@hotmail.com>
2024-05-02 09:13:48 -0300

    feat: add product page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,48 @@
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
+import ProductImage from "./_components/product-image";
+import ProductDetails from "./_components/product-detail";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProductsPage = async ({ params: { id } }: ProductPageProps) => {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      {/*Imagem*/}
+      <ProductImage product={product} />
+
+      {/* Titulo e Preço */}
+      <ProductDetails product={product} complementaryProducts={juices} />
+    </div>
+  );
+};
+
+export default ProductsPage;
