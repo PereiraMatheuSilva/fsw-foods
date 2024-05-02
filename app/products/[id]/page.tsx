609a506cbe11935b6f3448fb339d9faa9b67ecb2
Matheus Pereira <matheusp.dasilva@hotmail.com>
2024-05-02 09:16:04 -0300

    feat: ensure juices from product restaurants are listed

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -19,20 +19,23 @@ const ProductsPage = async ({ params: { id } }: ProductPageProps) => {
     },
   });
 
+  if (!product) {
+    return notFound();
+  }
+
   const juices = await db.product.findMany({
     where: {
       category: {
         name: "Sucos",
+      },
+      restaurant: {
+        id: product.restauranteId,
       },
     },
     include: {
       restaurant: true,
     },
   });
-
-  if (!product) {
-    return notFound();
-  }
 
   return (
     <div>
