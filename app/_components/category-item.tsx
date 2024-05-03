d4060f76b82fc847828a1e73fe45d55a0ba34cf0
Matheus Pereira <matheusp.dasilva@hotmail.com>
2024-05-03 08:36:27 -0300

    feat: add category products page

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -1,5 +1,6 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -7,9 +8,10 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <div
-      className="flex items-center justify-center gap-3 rounded-full bg-white px-4 
-    py-3 shadow-md"
+    <Link
+      href={`/categories/${category.id}/products`}
+      className="flex items-center justify-center gap-3
+      rounded-full bg-white px-4 py-3 shadow-md"
     >
       <Image
         src={category.imageUrl}
@@ -19,7 +21,7 @@ const CategoryItem = ({ category }: CategoryItemProps) => {
       />
 
       <p className="text-sm font-semibold">{category.name}</p>
-    </div>
+    </Link>
   );
 };
 
