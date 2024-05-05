50b5107c9330c51ac2dc45faba186a88af1cab41
Matheus Pereira <matheusp.dasilva@hotmail.com>
2024-05-05 16:01:14 -0300

    feat: add cart and cart item components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
 import "./globals.css";
+import { CartProvider } from "./_context/cart";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <CartProvider>{children}</CartProvider>
+      </body>
     </html>
   );
 }
