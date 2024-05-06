02a929f616e374cefebc65b27aa457ca0d3cc169
Matheus Pereira <matheusp.dasilva@hotmail.com>
2024-05-06 10:08:31 -0300

    refactor: total price and calculete

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -71,7 +71,8 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     }, 0);
   }, [products]);
 
-  const totalDiscounts = subTotalPrice - totalPrice;
+  const totalDiscounts =
+    subTotalPrice - totalPrice + Number(products?.[0]?.restaurant?.deliveryFee);
 
   const decreaseProductQuantity = (productId: string) => {
     return setProducts((prev) =>
